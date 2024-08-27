545940fa831992b3554c2ce3a1c957faf296d41a
a-fakeusername <71799509+a-fakeusername@users.noreply.github.com>
2024-08-27 13:26:57 -0500

    fixed the ui

--- src/components/ArmorComponent.tsx
+++ src/components/ArmorComponent.tsx
@@ -9,7 +9,8 @@ export default function ArmorComponent({armor}: Props) {
     return (
         <>
             <div className={armorName.split(" ")[0].toLowerCase()}>{armor.modifier && armor.modifier + " "}{armor.enchant && armor.enchant.name + " "}{armorName}</div>
-            {armor.jewels.length > 0 && <div>armor.jewels.join(" ")</div>};
+            <div>{armor.jewels.length > 0 && armor.jewels.join(" ")}</div>
+            <div className="br-small" />
         </>
     );
 }
--- src/components/BuildComponent.tsx
+++ src/components/BuildComponent.tsx
@@ -1,5 +1,6 @@
 import ArmorComponent from "./ArmorComponent";
 import { Build, StatOrder, statToIndex, getFormattedMultiplierStr, getBaseMult, getMultiplierColorStr, getNormalizedStats } from "../Backend.ts";
+import Stat from "./Stat.tsx";
 
 interface Props {
     build: Build;
@@ -12,11 +13,11 @@ export default function BuildComponent({ build }: Props) {
                 <div title="Multiplier of all stats, scales with weight">Multiplier: <span style={{ color: build.multiplierColorStr() }}>{getFormattedMultiplierStr(build.multiplier)}</span></div>
                 <div title="Multiplier from power and defense">Base Multiplier: <span style={{color: getMultiplierColorStr(getBaseMult(build))}}>{getFormattedMultiplierStr(getBaseMult(build))}</span></div>
                 <div title="Total stats, normalized to power">Efficiency Points: <span style={{color: getMultiplierColorStr(getNormalizedStats(build.stats) / 80)}}>{getFormattedMultiplierStr(getNormalizedStats(build.stats))}</span></div>
-                <div>{StatOrder.map(stat => build.stats[statToIndex[stat]] > 0 && <><span className={stat}>{build.stats[statToIndex[stat]]}</span><img className="icon" src="./armor/{stat}_icon.png" /></>).join(" ")}</div>
-                <div className="br-small"></div>
+                <div>{StatOrder.map(stat => build.stats[statToIndex[stat]] > 0 && <Stat key={statToIndex[stat]} index={statToIndex[stat]} value={build.stats[statToIndex[stat]]} />)}</div>
+                <div className="br-small" />
                 <div>
                     <h3>Armor</h3>
-                    {build.armorList.map(armor => <ArmorComponent armor={armor} />).join("")};
+                    {build.armorList.map((armor, i) => <ArmorComponent key={i} armor={armor} />)}
                 </div>
             </div>
         </>
