faeb49920de5496313188eb07b6a4751de34b4f8
myaltaccountsthis <71832770+myaltaccountsthis@users.noreply.github.com>
2024-09-01 18:50:10 -0500

    hopefully the last fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
+import 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/arcane-odyssey-guides/",
-  plugins: [react()],
+  plugins: [react()]
 })
