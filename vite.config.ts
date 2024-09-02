32111a9c9b53789ba417bcb040f8cb8ffc67d3e8
myaltaccountsthis <71832770+myaltaccountsthis@users.noreply.github.com>
2024-09-01 19:33:12 -0500

    maybe this fix works

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
-import 'path'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+declare const __dirname: string; // Add this line
+
 export default defineConfig({
   base: "/arcane-odyssey-guides/",
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        armor: path.resolve(__dirname, "armor/index.html"),
+        treasure: path.resolve(__dirname, "treasure/index.html"),
+      }
+    }
+  }
 })
