719906f083306bda5d44ea4767f436530ba173bf
myaltaccountsthis <71832770+myaltaccountsthis@users.noreply.github.com>
2024-11-03 21:38:22 -0600

    Update PathStuff.ts

--- src/PathStuff.ts
+++ src/PathStuff.ts
@@ -4,10 +4,12 @@ const paths = {
     treasure: "./treasure/"
 };
 
+/*
 if (window.location.hostname == "myaltaccountsthis.github.io") {
     paths.armor = "../armor/";
     paths.armorFile = "../armor/info.json";
     paths.treasure = "../treasure/"
 }
+*/
 
 export default paths;
